d70ef70d9235c771db0a1852c503a6a6e0cb8efc
kedycatsudo <dkocausta34@gmail.com>
2025-06-05 07:44:34 -0400

    sprint5 project stage 1

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,35 @@
+let initialCards = [
+  {
+    name: "Val Thorens",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg",
+  },
+
+  {
+    name: "Restaurant terrace",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg",
+  },
+
+  {
+    name: "outdoor cafe",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg",
+  },
+
+  {
+    name: "A very long bridge, over the forest and through the trees",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg",
+  },
+
+  {
+    name: "Tunnel with morning light",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg",
+  },
+
+  {
+    name: "Mountain house",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
+  },
+];
+
 const profileEditbtn = document.querySelector(`.profile__edit-btn`);
 const profileEditModal = document.querySelector(`#edit-profile-modal`);
 const profileClsBtn = profileEditModal.querySelector(`.modal__close-btn`);
@@ -52,4 +84,8 @@ profileAddBtn.addEventListener(`click`, function () {
 });
 newPostClsBtn.addEventListener(`click`, function () {
   newPostModal.classList.remove(`modal_is-opened`);
+});
+
+initialCards.forEach(function (card) {
+  console.log(card.name);
 });
